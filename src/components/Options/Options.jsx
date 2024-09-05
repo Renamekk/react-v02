122f80ab853a20529e18976e27147669369cc35b
Renamekk <black80512@gmail.com>
2024-09-05 20:00:02 +0200

    goit-react-hw-02

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,12 +1,12 @@
 import OptionsButton from "./OptionsButtom.jsx";
-import { Bad, Neutral, Smile } from "react-icons/bs";
+import { Bad, Neutral, BsEmojiSmile } from "react-icons/bs";
 import css from "./Options.module.css";
 
 export default function Options({ handler, reset, total }) {
   return (
     <div className={css.options}>
       <OptionsButton handlers={handler} type="good">
-        Good <Smile />
+        Good <BsEmojiSmile />
       </OptionsButton>
       <OptionsButton handlers={handler} type="neutral">
         Neutral <Neutral />{" "}
